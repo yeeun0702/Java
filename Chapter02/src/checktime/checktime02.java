5d014d3c80c623e93414dce50313dc1c38199133
yeeun0702 <kimyeeun0702@naver.com>
2023-12-11 04:56:39 +0900

    Chapter02 checktime02

---
+++ Chapter02/src/checktime/checktime02.java
@@ -0,0 +1,15 @@
+package checktime;
+
+public class checktime02 {
+
+	public static void main(String[] args) {
+		int i =4;
+		if (i % 2 ==0) {
+			if(i % 3 == 0)
+				System.out.println("6의 배수");}
+		else
+			System.out.println("홀수");
+
+	}
+
+}
