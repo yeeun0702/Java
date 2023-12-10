3659a51a51dd110b76abba1af48d06b613d4f989
yeeun0702 <kimyeeun0702@naver.com>
2023-12-10 13:48:39 +0900

    Chapter02 OpenChallenge

---
+++ Chapter02/src/OpenChallenge/RockScissorsPaper.java
@@ -0,0 +1,14 @@
+package OpenChallenge;
+import java.util.Scanner;
+
+public class RockScissorsPaper {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("가위바위보 게임입니다. 가위, 바위, 보 중에서 입력하세요.");
+
+		String 철수 = scanner.next();
+		String 영희 = scanner.next();
+	}
+
+}
