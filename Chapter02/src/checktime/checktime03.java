322acfb0197d559b24ab1d1f796990e234dc81d4
yeeun0702 <kimyeeun0702@naver.com>
2023-12-13 02:33:33 +0900

    Chapter02 checktime03

---
+++ Chapter02/src/checktime/checktime03.java
@@ -0,0 +1,23 @@
+package checktime;
+
+
+public class checktime03 {
+
+   public static void main(String[] args) {
+	   int option = 1;
+	   switch(option) {
+	   case 1:
+		   System.out.println("옵션 1");
+		   
+	   case 2:
+		   System.out.println("옵션 2");
+		   
+	   case 3:
+		   System.out.println("옵션 3");
+		   break;
+	   default:
+		   System.out.println("해당없음");
+	   }
+   
+}
+}
