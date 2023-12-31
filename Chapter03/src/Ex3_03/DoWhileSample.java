8f99837b732d471e84068b57f31f88beb931ef4e
yeeun0702 <kimyeeun0702@naver.com>
2024-01-01 02:04:15 +0900

    Chapter03 Ex3_03

---
+++ Chapter03/src/Ex3_03/DoWhileSample.java
@@ -0,0 +1,27 @@
+package Ex3_03;
+
+public class DoWhileSample {
+
+	public static void main(String[] args) {
+//		char c = 'a';
+//		do {
+//			System.out.print(c);
+//			c = (char) (c+1); // 영문의 경우 1을 더하면 다음 문자의 코드 값
+//		}
+//		while (c<= 'z');
+		
+//		char ch = 'a';
+//		while(ch <= 'z') {
+//			System.out.print(ch);
+////			ch = (char)(ch + 1);
+		
+		for (char i = 'a'; i <= 'z'; i ++) {
+			System.out.print(i);
+//
+		}
+		
+		
+	}
+}
+
+
