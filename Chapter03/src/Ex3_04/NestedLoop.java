fee337f9f6a2911399da06a35fe161708ca3cf74
yeeun0702 <kimyeeun0702@naver.com>
2024-01-03 22:29:04 +0900

    Chapter04 Ex3_04

---
+++ Chapter03/src/Ex3_04/NestedLoop.java
@@ -0,0 +1,16 @@
+package Ex3_04;
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		for(int i=1; i<10; i++) {
+			for(int j=1; j<10; j++) {
+				System.out.print(i + "x" + j + "=" + i*j);
+				System.out.print('\t');
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
