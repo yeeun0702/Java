4ca4f9f289406438aa9e74d4fd044ab0215ee1e6
yeeun0702 <kimyeeun0702@naver.com>
2024-01-04 02:07:27 +0900

    Chapter03 Ex3_05

---
+++ Chapter03/src/Ex3_05/ContinueExample.java
@@ -0,0 +1,25 @@
+package Ex3_05;
+
+import java.util.Scanner;
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("정수 5개를 입력하세요.");
+		int sum=0;
+		for(int i=0; i<5; i++) {
+			int n = scanner.nextInt();
+			if(n <=0 )
+				continue;
+			else
+				sum += n;
+		}
+		System.out.println("양수의 합은" + sum);
+		
+		scanner.close();
+
+	}
+
+}
