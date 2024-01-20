13266f0ba890d8458cb364160d460921f6c5c45e
yeeun0702 <kimyeeun0702@naver.com>
2024-01-21 02:29:47 +0900

    Chapter04 Ex4_2

---
+++ Chapter04/src/Ex4_2/RectApp.java
@@ -0,0 +1,23 @@
+package Ex4_2;
+
+import java.util.Scanner;
+
+class Rectangle{
+	int width;
+	int height; // 필드
+	public int getArea() {
+		return width*height;
+	}
+}
+
+public class RectApp {
+	public static void main(String[] args) {
+		Rectangle rect = new Rectangle(); // 객체 생성
+		Scanner scanner = new Scanner(System.in);
+		System.out.print(">>");
+		rect.width = scanner.nextInt();
+		rect.height = scanner.nextInt();
+		System.out.println("사각형의 면적은" + rect.getArea());
+		scanner.close();
+	}
+}
