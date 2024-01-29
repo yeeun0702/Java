c913615e0c31f90b3a1e4663e885c145e4405716
yeeun0702 <kimyeeun0702@naver.com>
2024-01-30 00:07:34 +0900

    Chapter04 Ex4_10

---
+++ Chapter04/src/Ex4_10/AccessEx.java
@@ -0,0 +1,18 @@
+package Ex4_10;
+
+class Sample{
+	public int a;
+	int c; }
+
+
+
+public class AccessEx {
+
+	public static void main(String[] args) {
+		Sample sample = new Sample();
+		sample.a = 10;
+		sample.c = 10;
+
+	}
+
+}
