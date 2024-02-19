ec638268fd6ece19bf837c3336798c223d2ef6fa
yeeun0702 <kimyeeun0702@naver.com>
2024-02-19 17:31:40 +0900

    Chpater04 practice Ex10

---
+++ Chapter04/src/practice/Ex10.java
@@ -0,0 +1,20 @@
+package practice;
+
+class Dictionary{
+	private static String[] kor = {"사랑", "아기", "돈", "미래", "희망"};
+	private static String[] eng = {"love", "baby", "money", "future", "hope"};
+//	public static String kor2Eng(String word) {
+//		
+//	}
+//	
+	
+}
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
