f017376b5c350ef69ad9e7fec47a231686e4ade9
yeeun0702 <kimyeeun0702@naver.com>
2024-03-03 23:58:39 +0900

    Chapter05 Ex5_07

---
+++ Chapter05/src/Ex5_7/GoodCalc.java
@@ -0,0 +1,38 @@
+package Ex5_7;
+
+
+abstract class Calculator {
+	public abstract int add(int a, int b);
+	public abstract int substract(int a, int b);
+	public abstract double average(int[] a);
+
+}
+
+public class GoodCalc extends Calculator {
+	
+	@Override
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	@Override
+	public int substract(int a, int b) {
+		return a-b;
+	}
+	
+	public double average(int[] a) {
+		double sum = 0;
+		for(int i=0; i<a.length; i++)
+			sum += a[i];
+		return sum/a.length;
+	}
+	public static void main(String[] args) {
+		Calculator c = new GoodCalc();
+		System.out.println(c.add(2,3));
+		System.out.println(c.substract(2,3));
+		System.out.println(c.average(new int [] {2,3,4}));
+		
+
+	}
+
+}
