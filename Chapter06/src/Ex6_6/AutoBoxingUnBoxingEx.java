276585dbad7a7d2a94fdc4aa4e75537aacc33ab0
yeeun0702 <kimyeeun0702@naver.com>
2024-04-08 23:41:30 +0900

    Chapter06 Ex6_06

---
+++ Chapter06/src/Ex6_6/AutoBoxingUnBoxingEx.java
@@ -0,0 +1,15 @@
+package Ex6_6;
+
+public class AutoBoxingUnBoxingEx {
+
+	public static void main(String[] args) {
+		int n = 10;
+		Integer intObject = n;
+		System.out.println("intObject = " + intObject);
+		
+		int m = intObject + 10;
+		System.out.println("m = " + m);
+
+	}
+
+}
