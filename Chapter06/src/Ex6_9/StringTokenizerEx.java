fda8045fab2d6d9006b416261e85fb1f79192ca1
yeeun0702 <kimyeeun0702@naver.com>
2024-04-13 21:35:20 +0900

    Chapter06 Ex6_09

---
+++ Chapter06/src/Ex6_9/StringTokenizerEx.java
@@ -0,0 +1,15 @@
+package Ex6_9;
+
+import java.util.StringTokenizer; 
+// control + shift + o
+public class StringTokenizerEx {
+
+	public static void main(String[] args) {
+		StringTokenizer st = new StringTokenizer("홍길동/장화/홍련/콩쥐/팥쥐", " /");
+		
+		while (st.hasMoreTokens())
+			System.out.println(st.nextToken());
+
+	}
+
+}
